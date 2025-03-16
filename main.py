369d395f41f1407aa83d2142321ae56378b4616b
Velavan5 <gvelavan2005@gmail.com>
2025-03-16 10:23:54 +0530

    Get path from CLI and Refactor file reading

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from stats import process
 from stats import calculate_letters
 from stats import sort_dict
+import sys
+
+def get_path():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else: 
+        return sys.argv[1]
+    
 
 def get_book_text(path):
-    with open(path) as file:
-        content = file.read()
+    try:
+        with open(path) as file:
+            content = file.read()
+    except FileNotFoundError:
+        print("File is not there: Check your path")
+        sys.exit(1)
+    except:
+        print("Error occured while reading the file Try again")
+        sys.exit(1)
     return content
 
 
 def main():
-    file_path = "./books/frankenstein.txt"
+    file_path = get_path()
     text = get_book_text(file_path)
-    no_words = process(text)
+    no_words = process(text) #calculates total words
     letters = calculate_letters(text)
     sorted_l = sort_dict(letters)
     print("============ BOOKBOT ============",
